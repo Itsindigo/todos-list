b4b9dfe28f353c210867e347bafe15a37ec78877
Matthew Bridges <matthewbridges@Matthews-MacBook-Pro-2.local>
2016-04-09 16:37:38 +0100

    Added Sign in logic

---
+++ todos/spec/features/user_creates_todo_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature 'User creates todo' do
+  scenario 'Successfully' do
+    sign_in
+    
+    click_on 'Create Todo'
+    fill_in 'Title', with: 'First todo'
+    click_on 'Submit'
+
+    expect(page).to have_css '.todos li', text: 'First todo'
+  end
+end
--- todos/spec/features/user_visits_homepage_spec.rb
+++ todos/spec/features/user_visits_homepage_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature 'User visits home page' do
   scenario 'Successfully' do
     visit root_path
-    expect(page).to have_css 'h1', 'text': 'Todos'
+    expect(page).to have_css 'h1', text: 'Todos'
   end
 end
